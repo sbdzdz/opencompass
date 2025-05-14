25e03fb036e274e9fae1be4d65eb13c3f87d4f5f
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2025-05-14 19:46:22 +0200

    Use local paths

--- opencompass/configs/models/internvl/lmdeploy_internvl_2_5_4b_base.py
+++ opencompass/configs/models/internvl/lmdeploy_internvl_2_5_4b_base.py
@@ -4,11 +4,11 @@ models = [
     dict(
         type=TurboMindModelwithChatTemplate,
         abbr='internvl2_5-4b-base-turbomind',
-        path='OpenGVLab/InternVL2_5-Pretrain-Models/tree/main/InternVL2_5-4B-Pretrain',
-        engine_config=dict(session_len=8192, max_batch_size=16, tp=1),
-        gen_config=dict(top_k=1, temperature=1e-6, top_p=0.9, max_new_tokens=8192),
-        max_seq_len=8192,
-        max_out_len=8192,
+        path='./models/InternVL2_5-4B-Pretrain',
+        engine_config=dict(session_len=16384, max_batch_size=16, tp=1),
+        gen_config=dict(top_k=1, temperature=1e-6, top_p=0.9, max_new_tokens=16384),
+        max_seq_len=16384,
+        max_out_len=16384,
         batch_size=16,
         run_cfg=dict(num_gpus=1),
     )
